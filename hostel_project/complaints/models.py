a75c80f278e117024804b20c51c70e94286b33c4
shamiiilll <shamilmami719@gmail.com>
2026-05-03 11:07:39 +0530

    Delete hostel_project directory

--- hostel_project/complaints/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from users.models import User
-
-class Complaint(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.TextField()
-    status = models.CharField(max_length=10, default='pending')
--- hostel_project/rooms/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Room(models.Model):
-    room_number = models.CharField(max_length=10)
-    capacity = models.IntegerField()
-    occupied = models.IntegerField(default=0)
-
-    def is_available(self):
-        return self.occupied < self.capacity
